a76d9350452d891f3c2dfa2e556a375cbf4b6501
Jack Madden <jck13mad@gmail.com>
2020-05-28 23:12:13 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,3 +1,11 @@
 class CreateShows < ActiveRecord::Base 
   def change 
-    create_table :shows
+    create_table :shows do |s|
+      s.string :name 
+      s.string :network
+      s.string :day 
+      s.rating :integer 
+      s.timestamp
+    end
+  end
+end
