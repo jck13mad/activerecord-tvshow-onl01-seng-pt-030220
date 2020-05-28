05bfb419f289cd89f5a12a8292ba126f9af1a997
Jack Madden <jck13mad@gmail.com>
2020-05-28 23:21:11 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,7 +1,10 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   
   def change
-    create_table if not EXIST 
+    create_table if not EXIST :shows do |s|
+      s.string :season 
+    end
+  end
   
   
 end
